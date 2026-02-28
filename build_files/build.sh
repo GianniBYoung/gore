90c65166fbd6b951b0d1321316794d78013041f1
Young, Gianni <cmancy@tutanota.com>
2026-02-28 09:57:07 -0600

    yazi deps

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,6 +4,14 @@ set -ouex pipefail
 
 dnf5 install -y fish
 
+# deps for yazi
+dnf5 install -y  \
+  ffmpeg \
+  p7zip p7zip-plugins \
+  poppler-utils \
+  ImageMagick \
+  wl-clipboard
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket nfs-server.service podman-auto-update.timer
